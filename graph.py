65a4b6b0f4bd22cb2f9754971ab6e207bf6f89a5
Eduardo Santos <eduardosantoshf@gmail.com>
2022-10-23 15:42:35 +0100

    Continued working on the assignment

--- graph.py
+++ graph.py
@@ -12,6 +12,10 @@ class SearchGraph:
         self.vertices = {}
         self.edges = {}
 
+    @property
+    def size(self):
+        return len(self.vertices)
+
     def add_vertice(self, vertice: Point, weight: int):
         self.vertices[vertice] = weight
 
@@ -23,24 +27,25 @@ class SearchGraph:
 
         return self
 
-    def random_graph(self, size: int, seed: int = 93107):
+    def random_graph(self, size: int, seed: int):
         self.vertices.clear()
         self.edges.clear()
 
         rand.seed(seed)
 
+        vertices = []
         for vertice in range(size):
             while True:
                 x, y = (rand.randint(1, 9), rand.randint(1, 9))
                 if not (x, y) in self.vertices: break
 
-            self.vertices.append((x, y))
+            vertices.append((x, y))
             self.add_vertice((x, y), rand.randint(1, 99))
 
         for v1 in range(size - 1):
             for _ in range(rand.randint(1, size // 2)):
                 v2 = rand.randint(v1 + 1, size - 1)
-                self.add_edge(self.vertices[v1], self.vertices[v2])
+                self.add_edge(vertices[v1], vertices[v2])
         
         return self
 
@@ -48,65 +53,45 @@ class SearchGraph:
         graph = nx.Graph()
 
         for vertices, neighbours in self.edges.items():
+            #print(vertices)
+            #print(neighbours)
             for neighbour in neighbours:
                 graph.add_edge(vertices, neighbour)
         
         positions = {x: x for x in graph.nodes}
+        print(positions)
 
-        low, *_, high = sorted(self.vertices.values())
-        norm = plot.colors.Normalize(vmin=low, vmax=high, clip=True)
-        mapper = plot.cm.ScalarMappable(norm=norm, cmap=plot.cm.coolwarm)
-        colors = [mapper.to_rgba(i) for i in self.vertices.values()]
-
-        sol_vertices = {}
-        sol_colors = []
-        rem_vertices = {}
-        rem_colors = []
-
-        for i, (v, w) in enumerate(self.vertices.items()):
-            if v in self.solution[1][0]:
-                sol_vertices[v] = w
-                sol_colors.append(colors[i])
-
-            else:
-                rem_vertices[v] = w
-                rem_colors.append(colors[i])
+        #low, *_, high = sorted(self.vertices.values())
+        #norm = plot.colors.Normalize(vmin=low, vmax=high, clip=True)
+        #mapper = plot.cm.ScalarMappable(norm=norm, cmap=plot.cm.coolwarm)
+        #colors = [mapper.to_rgba(i) for i in self.vertices.values()]
         
         nx.draw_networkx_edges(
             graph,
             pos=positions,
             nodelist=self.vertices,
             edge_color='gray',
-            node_size=500,
+            node_size=200,
             width=2,
-            ax=ax)
+            ax=ax
+        )
 
         nx.draw_networkx_nodes(
             graph,
             pos=positions,
-            labels=sol_vertices,
-            nodelist=sol_vertices,
-            node_size=800,
-            node_color=sol_colors,
-            cmap=plot.cm.summer,
-            ax=ax)
+            #labels=sol_vertices,
+            nodelist=self.vertices,
+            node_size=200,
+            #node_color=sol_colors,
+            #cmap=plot.cm.summer,
+            ax=ax
+        )
 
-        rem_nodes = nx.draw_networkx_nodes(
-            graph,
-            pos=positions,
-            labels=rem_vertices,
-            nodelist=rem_vertices,
-            node_size=500,
-            node_color='white',
-            cmap=plot.cm.summer,
-            ax=ax)
-        rem_nodes.set_edgecolors(rem_colors)
-        rem_nodes.set_linewidth(2)
-
-        nx.draw_networkx_labels(
-            graph,
-            pos=positions,
-            labels=self.vertices,
-            font_weight='bold',
-            font_color='black',
-            with_labels=True)
+        #nx.draw_networkx_labels(
+        #    graph,
+        #    pos=positions,
+        #    labels=self.vertices,
+        #    font_weight='bold',
+        #    font_color='black',
+        #    #with_labels=True
+        #)
---
+++ main.py
@@ -0,0 +1,65 @@
+import argparse
+from hashlib import algorithms_available
+import matplotlib.pyplot as plot
+from graph import SearchGraph
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description = 'Find a Minimum Weighted Closure for a Given Vertex-Weighted Directed Graph'
+    )
+    parser.add_argument(
+        '-f',
+        '--file',
+        metavar = 'FILE', 
+        type = argparse.FileType('r'),
+        help = 'Load a graph from a given file'
+    )
+    parser.add_argument(
+        '-r',
+        '--random',
+        metavar = 'SEED',
+        type = int,
+        help = 'Create a random graph given a seed'
+    )
+    parser.add_argument('-v',
+        '--vertices', 
+        metavar = 'N', 
+        default = 25, 
+        type = int, 
+        required = False,
+        help = 'number of vertices of the graph (default: %(default)s)'
+    )
+    parser.add_argument('-a', 
+        '--algorithm', 
+        metavar = 'NAME', 
+        default = 'exhaustive', 
+        type = str, 
+        required = False,
+        choices = ['exhaustive', 'branch-and-bound','greedy', 'astar', 'astar-heap'],
+        help = 'number of vertices of the graph (default: %(default)s)'
+    )
+    parser.add_argument('-hr',
+        '--heuristic', 
+        metavar = 'N', 
+        default = 1, 
+        type = int, 
+        required = False,
+        help = 'heuristic used by the Greedy and A-star approach: '
+            '(1) based on weights, (2) based on weights-degree,'
+            ' (default: %(default)s)')
+
+    args = vars(parser.parse_args())
+
+    seed = args["random"]
+    size = args["vertices"]
+    algorithm = args["algorithm"]
+    heuristic = args["heuristic"]
+
+    if seed:
+        g = SearchGraph().random_graph(size, seed)
+    else:
+        g = SearchGraph().read_graph(args["file"].name)
+
+    g.draw_graph()
+
+    plot.show()
